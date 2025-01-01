5eabf6da004ff6c2361e005684d6fe22318ad74a
Horvath Mark <horvath.mark1@students.jedlik.eu>
2025-01-02 00:52:58 +0100

    display the data of the movies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MovieModel } from './models/movie-model';
+import { DataService } from '../services/data.service';
 
 @Component({
   selector: 'app-root',
@@ -9,5 +11,19 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MoviesAngular';
+  movies: MovieModel[] = [];
+
+  constructor(private dataService: DataService) {}
+
+  ngOnInit(){
+    this.dataService.getMovies().subscribe({
+      next: (data: MovieModel[]) => {
+        this.movies = data;
+      },
+
+      error: (err) => console.log(err)
+
+    });
+  }
+
 }
